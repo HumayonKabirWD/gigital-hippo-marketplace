7df1cdf6c19086ad7c99a3f09c0b58989ccde1dd
kabir <kaabir24124@gmail.com>
2024-10-25 07:54:17 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Maxwidth from "@/components/Maxwidth";
 
 
 export default function Home() {
   return (
-    <div className="">
+    <Maxwidth>
+      <div className="py-20 mx-auto flex flex-col items-center text-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your Marketplace for High Quality{' '}<span className="text-violet-600">Digital Assets</span></h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">High quality Best Product for sevaral discount and Trusted for evry parspective.</p>
+      </div>
       
-    </div>
+    </Maxwidth>
   );
 }
